b2bff4077e7bb868be518fbf2d85d83a2bafc5aa
Dong Zhao <zd200572@gmail.com>
2023-04-01 20:20:22 +0800

    Update plugin_setup.py

--- q2_humann3/plugin_setup.py
+++ q2_humann3/plugin_setup.py
@@ -9,7 +9,7 @@ plugin = qiime2.plugin.Plugin(
     name='humann3',
     version=q2_humann3.__version__,
     website='http://huttenhower.sph.harvard.edu/humann2',
-    package='q2_humann2',
+    package='q2_humann3',
     user_support_text=("To get help with HUMAnN3, please post a question to "
                        "the HUMAnN Google Group form: "
                        "https://groups.google.com/forum/#!forum/humann-users"),
@@ -20,7 +20,7 @@ plugin = qiime2.plugin.Plugin(
 plugin.methods.register_function(
     function=q2_humann3.run,
     inputs={'demultiplexed_seqs': SampleData[SequencesWithQuality]},
-    parameters={'threads': qiime.plugin.Int},
+    parameters={'threads': qiime2.plugin.Int},
     name='Characterize samples using HUMAnN3',
     outputs=[('genefamilies', FeatureTable[Frequency]),
              ('pathcoverage', FeatureTable[RelativeFrequency]),
