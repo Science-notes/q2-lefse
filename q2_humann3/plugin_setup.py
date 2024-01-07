75188f06c1cf42c94b08a5fcb9e00f457546f3fa
Dong Zhao <zd200572@gmail.com>
2024-01-07 13:12:32 +0800

    Update plugin_setup.py
    1

--- q2_humann3/plugin_setup.py
+++ q2_humann3/plugin_setup.py
@@ -3,15 +3,15 @@ from q2_types.sample_data import SampleData
 from q2_types.per_sample_sequences import SequencesWithQuality
 from q2_types.feature_table import Frequency, RelativeFrequency, FeatureTable
 
-import q2_humann3
+import q2_lefse
 
 
 plugin = qiime2.plugin.Plugin(
-    name='humann3',
-    version=q2_humann3.__version__,
+    name='lefse',
+    version=q2_lefse.__version__,
     website='http://huttenhower.sph.harvard.edu/humann2',
-    package='q2_humann3',
-    user_support_text=("To get help with HUMAnN3, please post a question to "
+    package='q2_lefse',
+    user_support_text=("To get help with lefse, please post a question to "
                        "the HUMAnN Google Group form: "
                        "https://groups.google.com/forum/#!forum/humann-users"),
     citation_text=None
@@ -19,12 +19,23 @@ plugin = qiime2.plugin.Plugin(
 
 
 plugin.methods.register_function(
-    function=q2_humann3.run,
+    function=q2_lefse.run,
     inputs={'demultiplexed_seqs': SampleData[SequencesWithQuality]},
     parameters={'threads': qiime2.plugin.Int},
-    name='Characterize samples using HUMAnN3',
+    name='Linear discriminant analysis Effect Size analysis using lefse',
     outputs=[('genefamilies', FeatureTable[Frequency]),
              ('pathcoverage', FeatureTable[RelativeFrequency]),
              ('pathabundance', FeatureTable[RelativeFrequency])],
-    description='Execute the HUMAnN3'
+    description='Execute the lefse'
+)
+
+plugin.visualizers.register_function(
+    function=q2_lefse.run,
+    inputs={'demultiplexed_seqs': SampleData[SequencesWithQuality]},
+    parameters={'threads': qiime2.plugin.Int},
+    name='Linear discriminant analysis Effect Size analysis using lefse',
+    outputs=[('genefamilies', FeatureTable[Frequency]),
+             ('pathcoverage', FeatureTable[RelativeFrequency]),
+             ('pathabundance', FeatureTable[RelativeFrequency])],
+    description='Execute the lefse'
 )
