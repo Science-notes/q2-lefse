b84f11b24d2b39326e1ba0c3cd1927fb26c71a76
Dong Zhao <zd200572@gmail.com>
2024-01-07 15:31:09 +0800

    Create _otu.py
    add otu table type

---
+++ q2_lefse/_otu.py
@@ -0,0 +1,22 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2023-, zd200572 & QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from qiime2.plugin import SemanticType, model
+from q2_types.sample_data import SampleData
+
+
+DADA2Stats = SemanticType('OTUTable', variant_of=SampleData.field['type'])
+
+
+class DADA2StatsFormat(model.TextFileFormat):
+    def validate(*args):
+        pass
+
+
+OTUTableDirFmt = model.SingleFileDirectoryFormat(
+    'OTUTableDirFmt', 'otu.txt', OTUTableFormat)
