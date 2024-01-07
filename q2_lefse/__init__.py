ac638cbde57bc29ebf54902ce55d73c3d81c1179
Dong Zhao <zd200572@gmail.com>
2024-01-07 16:25:55 +0800

    Update __init__.py

--- q2_lefse/__init__.py
+++ q2_lefse/__init__.py
@@ -3,4 +3,4 @@ from ._primary import run
 __version__ = "0.0.3"
 from ._otu import OTUTable, OTUTableDirFmt, OTUTableFormat
 
-__all__ = ['run']
+__all__ = ['run', 'OTUTable', 'OTUTableDirFmt', 'OTUTableFormat']
